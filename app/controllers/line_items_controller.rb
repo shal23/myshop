43552c47fc8981e7d6b9852c44ee38aec997a57e
shal23 <dr.shalinidalal@gmail.com>
2013-06-02 18:54:26 +1000

    commit

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,4 @@
-class LineItemsController < ApplicationController
-  # GET /line_items
+# GET /line_items
   # GET /line_items.json
   def index
     @line_items = LineItem.all
@@ -40,11 +39,14 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(params[:line_item])
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
